de5dea265b508f39bf589ed9364e89bdf493a2c1
Shuhaib <shuhaib.2019678@iit.ac.lk>
2021-07-23 22:25:38 +0530

    Black-end Commit

---
+++ spring-employee(Backend)/src/main/java/com/spring/backend/exception/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.spring.backend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionID = 1L;
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ spring-employee(Backend)/src/main/java/com/spring/backend/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.spring.backend.repository;
+
+import com.spring.backend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
